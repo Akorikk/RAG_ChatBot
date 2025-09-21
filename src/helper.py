8e197d9a9bb6dc381d1adeaaba3f4d161cf56c00
Abhishek Kori <abhishekkori601@gmail.com>
2025-09-21 18:49:56 +0530

    helper.py

--- src/helper.py
+++ src/helper.py
@@ -2,6 +2,7 @@ from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from typing import List
 from langchain.schema import Document
+from langchain.embeddings import HuggingFaceEmbeddings
 
 # Extract text from PDF files
 def load_pdf_file(data):
@@ -29,4 +30,22 @@ def filter_doc(doc: List[Document]) -> List[Document]:
                 metadata={"source": src}
             )
         )
-    return minimal_docs    
+    return minimal_docs  
+
+# Split the documents into samller Chunks
+def text_split(minimal_docs):
+    text_spliter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+    )
+    texts_chunk = text_spliter.split_documents(minimal_docs)
+    return texts_chunk
+
+
+def download_embeddings():
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name,
+    )
+    return embeddings
+embedding = download_embeddings()
